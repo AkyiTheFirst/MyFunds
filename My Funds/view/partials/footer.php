f4791088496c622c084361fddffcd06fb5877576
AkyiTheFirst <86602063+AkyiTheFirst@users.noreply.github.com>
2024-08-07 20:23:35 +0630

    Delete My Funds directory

--- My Funds/view/partials/footer.php
+++
@@ -1,9 +0,0 @@
-<!-- footer -->
-<footer class="footer footer-center bg-primary text-base-100 p-3">
-    <aside>
-        <p>Copyright © 2024 - All right reserved by AkyiTheFirst</p>
-    </aside>
-</footer>
-</body>
-
-</html>
